94adadee014db2d86cc9f84288d1afb9abb2c4df
Tony <tstaunton@taxback.com>
2011-04-16 23:54:23 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aphrodite::Application.config.secret_token = '444282ff35718aaa8a38c11eacc64d8b39772271270c9ed4a77d3c5979989ce2a75b8834e75196cdb768de40d8003a7bea879498914a11318f6e760cccfe9e63'
